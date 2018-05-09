c42fa407f19b54839381273e736d1ba2ea4be211
EC2 Default User <ec2-user@ip-172-31-13-83.us-west-2.compute.internal>
2018-05-09 15:43:16 +0000

    Servidor de Licencias se agrego Organization.java

---
+++ src/main/java/com/gfi/licensingservice/model/Organization.java
@@ -0,0 +1,10 @@
+package com.gfi.licensingservice.model;
+
+public class Organization {
+    
+    private String id;
+    private String name;
+    private String contactName;
+    private String contactEmail;
+    private String contactPhone;
+}
