a37e5f9592ccb63c73ab9b2ef29c2827cc2fd929
gusdelact <ec2-user@ip-172-31-13-83.us-west-2.compute.internal>
2018-05-09 16:26:19 +0000

    se agrego controlador REST - LicenseServiceController.java

---
+++ src/main/java/com/gfi/licensingservice/controllers/LicenseServiceController.java
@@ -0,0 +1,42 @@
+package com.gfi.licensingservice.controllers;
+
+import com.gfi.licensingservice.model.License;
+import com.gfi.licensingservice.services.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@RestController
+@RequestMapping(value="v1/organizations/{organizationId}/licenses")
+public class LicenseServiceController {
+    @Autowired
+    private LicenseService licenseService;
+
+    @RequestMapping(value="/{licenseId}",method = RequestMethod.GET)
+    public License getLicenses( @PathVariable("organizationId") String organizationId,
+                                @PathVariable("licenseId") String licenseId) {
+
+        return licenseService.getLicense(licenseId);
+      
+    }
+
+    @RequestMapping(value="{licenseId}",method = RequestMethod.PUT)
+    public String updateLicenses( @PathVariable("licenseId") String licenseId) {
+        return String.format("PUT para actualizar");
+    }
+
+    @RequestMapping(value="{licenseId}",method = RequestMethod.POST)
+    public String saveLicenses( @PathVariable("licenseId") String licenseId) {
+        return String.format("POST para crear");
+    }
+
+    @RequestMapping(value="{licenseId}",method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public String deleteLicenses( @PathVariable("licenseId") String licenseId) {
+        return String.format("DEL para borrar");
+    }
+}
